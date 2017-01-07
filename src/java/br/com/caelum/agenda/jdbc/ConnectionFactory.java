6974d261ed1255dca207d4cafa963fbd57825d07
Gilvan Almeida <g6almeida@hotmail.com>
2017-01-07 00:02:38 -0200

    Ajustando a conexão com o banco de dados

--- src/java/br/com/caelum/agenda/jdbc/ConnectionFactory.java
+++ src/java/br/com/caelum/agenda/jdbc/ConnectionFactory.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 public class ConnectionFactory{
     public Connection getConnection(){
         try {
+            Class.forName("com.mysql.jdbc.Driver");
             return DriverManager.getConnection("jdbc:mysql://localhost/fj21", "root", "root");
         } catch (Exception e) {
             throw new RuntimeException(e);
